37fc750d51dd688032060ae0511ed4c36ccb0972
Giscard AKINERINLA <isidoreakinerinla@gmail.com>
2025-08-12 15:59:50 +0100

    Update

--- push_that.sh
+++ push_that.sh
@@ -2,4 +2,5 @@ read -p "Entrez le message de votre commit: " commitMessage
 git commit -am $commitMessage
 git add -A
 git push
-echo "Push réussi et voici son nom: $commitMessage."
+echo "Push réussi !!!"
+echo "Nom du commit: $commitMessage."
