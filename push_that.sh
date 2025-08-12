2325e6e27d06e7258dfac22cd56b79c59bd9a97b
Giscard AKINERINLA <isidoreakinerinla@gmail.com>
2025-08-12 16:51:08 +0100

    Improve

--- push_that.sh
+++ push_that.sh
@@ -2,5 +2,21 @@ read -p "Entrez le message de votre commit: " commitMessage
 git commit -am $commitMessage
 git add -A
 git push
-echo "Push réussi !!!"
+if test "$?" = "0"
+then
+    echo "Push réussi avec succés !!! "
+else
+    echo "Tentative de push échouée. Essayons de réparer si possible les erreurs."
+    git pull origin main
+    if test "$?" != "0"
+    then
+	while test "$?" != "0"
+        do
+          echo "Echec de nouveau."
+          git pull --rebase
+	  echo "J'ai fait une rebase."
+          git push
+        done
+     else
+	echo "Push réussi !!"
 echo "Nom du commit: $commitMessage."
