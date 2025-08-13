bdb2662d8114721a31df6e7e5fc1386085f666a6
Giscard AKINERINLA <isidoreakinerinla@gmail.com>
2025-08-13 08:39:14 +0100

    EssaiRéussi

--- push_that.sh
+++ push_that.sh
@@ -8,17 +8,11 @@ then
 else
     echo "Tentative de push échouée. Essayons de réparer si possible les erreurs."
     git pull --rebase origin main
-    
-    if test "$?" != "0"
-    then
-		while test "$?" != "0"
-		do
-			echo "Echec de nouveau."
-        done
-    else
-	echo "Push réussi !!"
-    echo "Nom du commit: $commitMessage"
-    fi
+    while test "$?" != "0"
+	do
+		echo "Echec de nouveau."
+	done
+
 fi
 
 echo "Push réussi !!!"
